43342b50d227d7f5f9eb26fd6b4ab1a1feda9f72
D3f4ult <cvdjb8@inf.elte.hu>
2025-05-16 10:52:50 +0200

    feat: delete unnecessary files

--- src/components/Navbar/Logo.tsx
+++
@@ -1,4 +0,0 @@
-import Image from 'next/image'
-export function Logo() {
-  return <Image src="/logo.png" alt="EYP Camp Logo" width={48} height={48} />
-}
--- src/components/SectionHeading/SectionHeading.tsx
+++
@@ -1,3 +0,0 @@
-export function SectionHeading({ children }: { children: React.ReactNode }) {
-    return <h2 className="text-3xl font-bold text-forest-dark mb-4">{children}</h2>
-  }
