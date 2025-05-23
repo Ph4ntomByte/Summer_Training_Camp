303529f39df33301420a2e4125fa2a57cabb3d4c
D3f4ult <cvdjb8@inf.elte.hu>
2025-05-23 15:45:15 +0200

    chore: add footer for all the pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navigation/Navbar";
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import BackToTopButton from '@/components/UI/BackToTopButton'
-
+import { Footer } from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +25,7 @@ export default function RootLayout({
         <Navbar />
         <div className="pt-16">
           {children}
+          <Footer />
           <Analytics />
           <SpeedInsights />
           <BackToTopButton />
